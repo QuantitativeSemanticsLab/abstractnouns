666d8f4769b21be3fe35a2f07c40af4b466c2cea
txstc55 <txstc55@gmail.com>
2017-05-30 13:06:52 -0400

    added more commends and can directly parse COCA

--- parsing/parseCorpus.py
+++ parsing/parseCorpus.py
@@ -37,95 +37,123 @@ params = (
 
 
 def parseDependency(dep_list):
-	# We are reading from a list, where all the items are 
-	# dictionaries, we just have to extract all the information
-	# from the dictionaries
+  # We are reading from a list, where all the items are 
+  # dictionaries, we just have to extract all the information
+  # from the dictionaries
 
-	parsedDependencyList=[]
-	for dependency in dep_list:
-		dep=dependency['dep']
-		dependent=dependency['dependent']
-		dependentGloss=dependency['dependentGloss']
-		governorGloss=dependency['governorGloss']
-		governor=dependency['governor']
-		dependencyText=str(dep)+'('+str(governorGloss)+'-'+str(governor)+', '+str(dependentGloss)+'-'+str(dependent)+')'
-		parsedDependencyList.append(dependencyText)
-	return parsedDependencyList
+  parsedDependencyList=[]
+  for dependency in dep_list:
+    dep=dependency['dep']
+    dependent=dependency['dependent']
+    dependentGloss=dependency['dependentGloss']
+    governorGloss=dependency['governorGloss']
+    governor=dependency['governor']
+    dependencyText=str(dep)+'('+str(governorGloss)+'-'+str(governor)+', '+str(dependentGloss)+'-'+str(dependent)+')'
+    parsedDependencyList.append(dependencyText)
+  return parsedDependencyList
 
 
 def parsePOS(pos_list):
-	# parsing the part of speech tags
-	sentence=[]
-	posList=[]
-	for tags in pos_list:
-		word=tags['word']
-		pos=tags['pos']
-		posList.append(word+'/'+pos)
-		sentence.append(word)
-	return [' '.join(sentence),' '.join(posList)]
+  # parsing the part of speech tags
+  sentence=[]
+  posList=[]
+  for tags in pos_list:
+    word=tags['word']
+    pos=tags['pos']
+    posList.append(word+'/'+pos)
+    sentence.append(word)
+  return [' '.join(sentence),' '.join(posList)]
 
 def parseSentence(sentence):
-	rows=[]
-	data=sentence.replace(u'\xa0', u' ').encode('utf-8')
-	try: 
-		result= requests.post('http://localhost:9000/', params=params, data=data).content
-		json_item = json.loads(result)
-		for item in json_item['sentences']:
-			dep_list=item['enhancedPlusPlusDependencies']
-			pos_list=item['tokens']
-			pos_result=parsePOS(pos_list)
-			dependency_result=parseDependency(dep_list)
-			rows.append([pos_result[0],pos_result[1]," ".join(dependency_result)])
-			# print " ".join(dependency_result)
-		return rows
-	except:
-		print "Error parsing sentence"
-		return False
+  # this function is to parse a sentence
+  # so what I did is to send the sentence to local server
+  # if you run the code I provided at the top,
+  # then the port is 9000. You can of course 
+  # use another one if port 9000 is busy
+  rows=[]
+  
+  try: 
+    # parsing to utf-8 is going to cause some problems in some lines
+    # I have not done specific work to parse a single sentence each time
+    # this is parsing a line in the text file
+    # it is hard to tell where a sentence ends. It can be ?,.! or whatever
+    # leave it to the stanford parser
+    data=sentence.replace(u'\xa0', u' ').encode('utf-8')
+    result= requests.post('http://localhost:9000/', params=params, data=data).content
+    json_item = json.loads(result)
+    for item in json_item['sentences']:
+      dep_list=item['enhancedPlusPlusDependencies']
+      pos_list=item['tokens']
+      pos_result=parsePOS(pos_list)
+      dependency_result=parseDependency(dep_list)
+      rows.append([pos_result[0],pos_result[1]," ".join(dependency_result)])
+      # print " ".join(dependency_result)
+    return rows
+  except:
+    print "Error parsing sentence"
+    return False
 
 def parseFile(file):
-	file_name=os.path.basename(file)
-	path_name=os.path.dirname(file)
-	f=open(file,'r')
-	print "File read"
-	filetup = re.findall(r'(\w*).txt', file_name)
-	csv_name=path_name+"/"+filetup[0]+'.csv'
-	print "CSV path: %s"%csv_name
-	with open(csv_name,'wb') as outcsv:
-		print "CSV created"
-		print "Start parsing"
-		writer=csv.writer(outcsv)
-		writer.writerow(['sent','tags','parse'])
-		line_num=1
-		for line in f:
-			rows=parseSentence(line)
-			if rows:
-				for row in rows:
-					writer.writerow(row)
-			print "File %s line %d finished"%(file_name,line_num)
-			line_num=line_num+1
-	print "File finished %s"%csv_name
+  # this function parses a file
+  file_name=os.path.basename(file)
+  path_name=os.path.dirname(file)
+  f=open(file,'r')
+  print "File read"
+  filetup = re.findall(r'(\w*).txt', file_name)
+  csv_name=path_name+"/"+filetup[0]+'.csv'
+  print "CSV path: %s"%csv_name
+  with open(csv_name,'wb') as outcsv:
+    print "CSV created"
+    print "Start parsing"
+    writer=csv.writer(outcsv)
+    writer.writerow(['sent','tags','parse'])
+    line_num=1
+    for line in f:
+      rows=parseSentence(line)
+      if rows:
+        for row in rows:
+          writer.writerow(row)
+      print "File %s line %d finished"%(file_name,line_num)
+      line_num=line_num+1
+  print "File finished %s"%csv_name
 
+
+
+def mainWork(inFile):
+  # Check the input. if it is a directory, then parse each file in the directory
+  # if it is a directory, then it will browse all files in it
+  if os.path.isfile(inFile):
+    parseFile(inFile)
+  else:
+    print inFile
+    f=[]
+    for (dirpath, dirnames, filenames) in walk(inFile):
+      f.extend(filenames)
+      break
+    f=filter(lambda x: x.endswith('.txt'),f)
+    f=map(lambda x: inFile+'/'+x, f)
+    pool=Pool(cpu_count())
+    print "Start multi processing"
+    start = timeit.default_timer()
+    pool.map(parseFile,f)
+    stop = timeit.default_timer()
+
+    print stop - start 
 
 
 inFile=sys.argv[1]
-
-if os.path.isfile(inFile):
-	parseFile(inFile)
-else:
-	print inFile
-	f=[]
-	for (dirpath, dirnames, filenames) in walk(inFile):
-		f.extend(filenames)
-		break
-	f=filter(lambda x: x.endswith('.txt'),f)
-	f=map(lambda x: inFile+'/'+x, f)
-	pool=Pool(cpu_count())
-	print "Start multi processing"
-	start = timeit.default_timer()
-	pool.map(parseFile,f)
-	stop = timeit.default_timer()
-
-	print stop - start 
+directory_list=[]
+for root, dirs, files in walk(inFile):
+  directory_list.extend(dirs)
+  # Added this line so that if there are text files in the directory
+  # then it will also be parsed directly
+  # I have not run tthis code with this specific line, but should be fine       
+  directory_list.exted(files)
+  break
+directory_list=map(lambda x: inFile+"/"+x, directory_list)
+for dirs in directory_list:
+  print "Start working in %s"%dirs
+  mainWork(dirs)
 
 
 
